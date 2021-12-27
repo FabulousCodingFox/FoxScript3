c859959ac63339a02d07d8e3f7648cce82bb809d
FabulousFox <78906517+FabulousCodingFox@users.noreply.github.com>
2021-12-27 02:24:56 +0100

    Update main.py

--- FoxScript3/main.py
+++ FoxScript3/main.py
@@ -1,7 +1,7 @@
 import Compiler
 
-projectPath = "C:/Users/fabif/Desktop/FoxScriptV3/project-example"
-compilePath = "C:/Users/fabif/Desktop/FoxScriptV3/compiled"
+projectPath = ".."
+compilePath = ".."
 
 compiler = Compiler.Compiler(projectPath,compilePath)
 compiler.readProjectFiles()
