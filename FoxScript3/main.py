a1f5ef09bda2a5e1a74b7688fc46ba61166cb86f
FabulousFox <78906517+FabulousCodingFox@users.noreply.github.com>
2021-12-27 11:56:11 +0100

    Delete main.py

--- FoxScript3/main.py
+++
@@ -1,9 +0,0 @@
-import Compiler
-
-projectPath = "C:/Users/fabif/Desktop/FoxScriptV3/project-example"
-compilePath = "C:/Users/fabif/Desktop/FoxScriptV3/compiled"
-
-compiler = Compiler.Compiler(projectPath,compilePath)
-compiler.readProjectFiles()
-compiler.compile()
-compiler.create()
